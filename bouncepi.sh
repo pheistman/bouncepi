35a47d79716245aee1bdac3fd24b7df40107f857
pheistman <pheistman@live.co.uk>
2019-07-30 09:52:39 +0100

    Edited shell script to remove extra lines

---
+++ bouncepi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#This script will stop and start pihole-FTL and restart the pihole DNS
+
+echo $'\n'$"You are about to restart pihole services..."
+echo "+"
+echo "+"
+echo "+"
+echo "+"
+echo $'\n'$"Stopping pihole-FTL..."
+sudo service pihole-FTL stop
+
+echo $'\n'$"Starting pihole-FTL..."
+sudo service pihole-FTL start
+echo 
+sudo pihole restartdns
